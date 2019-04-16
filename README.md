564f259b8bb233d65b58593504768e82e7b39030
李欢 <li.huan@synyi.com>
2019-04-16 17:27:20 +0800

    add http

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # node-roadmap
 node 学习记录
+
+#### 1：初始化yarn
+
+~~~shell
+$ yarn init
+~~~
+
+
+
+#### 2：添加入口文件index.js，添加项目代码和资源目录
+
+~~~shell
+$ > index.js
+$ mkdir src assets
+~~~
+
+
+
+#### 3.添加jshint检查
+
+yarn
+
+~~~shell
+$ yarn add jshint -D
+~~~
+
+npm
+
+~~~shell
+$ npm install jshint -s -d
+~~~
+
+添加`.jshintrc`和`.jshintignore`文件
+
+~~~shell
+$ >.jshintrc .jshintignore 
+~~~
+
+然后在.jshintrc中添加配置项，可以参照[文档](<https://github.com/jshint/node-jshint/blob/master/.jshintrc>)，在.jshintignore中添加不需要检查的文件路径，
+
+
+
+至此，全局设置基本结束。接下来可以根据以下目录跳转到不同页面，查看示例。
+
+
+
+#### 目录
+
---
+++ src/http/http-server.js
@@ -0,0 +1,62 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8085;
+const HOST = '127.0.0.1';
+
+const server = http.createServer((req, res) => {
+  // logRequest(req, res);
+  requestHandle(req, res);
+
+}).listen(PORT, HOST);
+
+function logRequest(req, res) {
+  if (req.url !== '/favicon.ico') {
+    const logFilePath = path.resolve(__dirname, '../../assets/log/request.log')
+    const out = fs.createWriteStream(logFilePath);
+    out.write(`客户端请求所用方法为: ${req.method}\r\n`);
+    out.write(`客户端请求所用url字符串为: ${req.url}\r\n`);
+    out.write(`客户端请求头对象为: ${JSON.stringify(req.headers)}\r\n`);
+    out.end(`客户端请求所用HTTP版本为: ${req.httpVersion}`);
+  }
+}
+
+function requestHandle(req, res) {
+  if (req.url !== '/favicon.ico') {
+    req.on('data', (data) => {
+      console.log(`服务器接收到数据：${decodeURIComponent(data)}`);
+    });
+
+    req.on('end', () => {
+      console.log('客户端请求数据已全部接受完毕！');
+    });
+  }
+  res.end();
+}
+
+server.on('listening', () => {
+  console.log('服务器开始监听。');
+  // server.close();
+});
+
+// 设置响应超时时间
+// server.setTimeout(10 * 1000);
+
+server.on('connection', (socket) => {
+  console.log('客户端已链接...');
+});
+
+server.on('timeout', (socket) => {
+  console.log('服务器超时!');
+});
+
+server.on('close', () => {
+  console.error('服务器已经被关闭。');
+});
+
+server.on('error', err => {
+  if (err.code === 'EADDRINUSE') {
+    console.log('ERROR: 指定地址或端口者已经被占用！');
+  }
+});
